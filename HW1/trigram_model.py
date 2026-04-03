8f8b2ff901ed1a46ed710c53f43b3bce5e074e2f
S2chloeS2 <cl4490@columbia.edu>
2026-04-03 11:14:57 -0400

    Restore all HW files

---
+++ HW1/trigram_model.py
@@ -0,0 +1,201 @@
+import sys
+from collections import defaultdict
+import math
+import random
+import os
+import os.path
+"""
+COMS W4705 - Natural Language Processing - Spring 2026
+Programming Homework 1 - Trigram Language Models
+Daniel Bauer
+"""
+
+def corpus_reader(corpusfile, lexicon=None): 
+    with open(corpusfile,'r') as corpus: 
+        for line in corpus: 
+            if line.strip():
+                sequence = line.lower().strip().split()
+                if lexicon: 
+                    yield [word if word in lexicon else "UNK" for word in sequence]
+                else: 
+                    yield sequence
+
+def get_lexicon(corpus):
+    word_counts = defaultdict(int)
+    for sentence in corpus:
+        for word in sentence: 
+            word_counts[word] += 1
+    return set(word for word in word_counts if word_counts[word] > 1)  
+
+
+
+def get_ngrams(sequence, n):
+    """
+    COMPLETE THIS FUNCTION (PART 1)
+    Given a sequence, this function should return a list of n-grams, where each n-gram is a Python tuple.
+    This should work for arbitrary values of n >= 1
+    """
+    padded = ["START"] * (n - 1) + list(sequence) + ["STOP"]
+    return [tuple(padded[i:i+n]) for i in range(len(padded) - n + 1)]
+
+
+class TrigramModel(object):
+    
+    def __init__(self, corpusfile):
+    
+        # Iterate through the corpus once to build a lexicon 
+        generator = corpus_reader(corpusfile)
+        self.lexicon = get_lexicon(generator)
+        self.lexicon.add("UNK")
+        self.lexicon.add("START")
+        self.lexicon.add("STOP")
+    
+        # Now iterate through the corpus again and count ngrams
+        generator = corpus_reader(corpusfile, self.lexicon)
+        self.count_ngrams(generator)
+
+
+    def count_ngrams(self, corpus):
+        """
+        COMPLETE THIS METHOD (PART 2)
+        Given a corpus iterator, populate dictionaries of unigram, bigram,
+        and trigram counts. 
+        """
+   
+        self.unigramcounts = defaultdict(int)
+        self.bigramcounts = defaultdict(int)
+        self.trigramcounts = defaultdict(int)
+        self.total_words = 0
+
+        for sentence in corpus:
+            self.total_words += len(sentence) + 1  # +1 for STOP token
+            for unigram in get_ngrams(sentence, 1):
+                self.unigramcounts[unigram] += 1
+            for bigram in get_ngrams(sentence, 2):
+                self.bigramcounts[bigram] += 1
+            for trigram in get_ngrams(sentence, 3):
+                self.trigramcounts[trigram] += 1
+
+        return
+
+    def raw_trigram_probability(self,trigram):
+        """
+        COMPLETE THIS METHOD (PART 3)
+        Returns the raw (unsmoothed) trigram probability
+        """
+        bigram_context = trigram[:2]
+        bigram_count = self.bigramcounts[bigram_context]
+        if bigram_count == 0:
+            return 0.0
+        return self.trigramcounts[trigram] / bigram_count
+
+    def raw_bigram_probability(self, bigram):
+        """
+        COMPLETE THIS METHOD (PART 3)
+        Returns the raw (unsmoothed) bigram probability
+        """
+        unigram_context = (bigram[0],)
+        unigram_count = self.unigramcounts[unigram_context]
+        if unigram_count == 0:
+            return 0.0
+        return self.bigramcounts[bigram] / unigram_count
+
+    def raw_unigram_probability(self, unigram):
+        """
+        COMPLETE THIS METHOD (PART 3)
+        Returns the raw (unsmoothed) unigram probability.
+        """
+        return self.unigramcounts[unigram] / self.total_words
+
+    def generate_sentence(self,t=20): 
+        """
+        COMPLETE THIS METHOD (OPTIONAL)
+        Generate a random sentence from the trigram model. t specifies the
+        max length, but the sentence may be shorter if STOP is reached.
+        """
+        return result            
+
+    def smoothed_trigram_probability(self, trigram):
+        """
+        COMPLETE THIS METHOD (PART 4)
+        Returns the smoothed trigram probability (using linear interpolation). 
+        """
+        lambda1 = 1/3.0
+        lambda2 = 1/3.0
+        lambda3 = 1/3.0
+        return lambda1 * self.raw_trigram_probability(trigram) + \
+               lambda2 * self.raw_bigram_probability(trigram[1:]) + \
+               lambda3 * self.raw_unigram_probability((trigram[2],))
+        
+    def sentence_logprob(self, sentence):
+        """
+        COMPLETE THIS METHOD (PART 5)
+        Returns the log probability of an entire sequence.
+        """
+        trigrams = get_ngrams(sentence, 3)
+        logprob = 0.0
+        for trigram in trigrams:
+            logprob += math.log2(self.smoothed_trigram_probability(trigram))
+        return logprob
+
+    def perplexity(self, corpus):
+        """
+        COMPLETE THIS METHOD (PART 6) 
+        Returns the log probability of an entire sequence.
+        """
+        total_logprob = 0.0
+        total_words = 0
+        for sentence in corpus:
+            total_logprob += self.sentence_logprob(sentence)
+            total_words += len(sentence) + 1  # +1 for STOP
+        l = total_logprob / total_words
+        return 2 ** (-l)
+
+
+def essay_scoring_experiment(training_file1, training_file2, testdir1, testdir2):
+
+        model1 = TrigramModel(training_file1)
+        model2 = TrigramModel(training_file2)
+
+        total = 0
+        correct = 0       
+ 
+        for f in os.listdir(testdir1):
+            pp1 = model1.perplexity(corpus_reader(os.path.join(testdir1, f), model1.lexicon))
+            pp2 = model2.perplexity(corpus_reader(os.path.join(testdir1, f), model2.lexicon))
+            total += 1
+            if pp1 <= pp2:
+                correct += 1
+
+        for f in os.listdir(testdir2):
+            pp1 = model1.perplexity(corpus_reader(os.path.join(testdir2, f), model1.lexicon))
+            pp2 = model2.perplexity(corpus_reader(os.path.join(testdir2, f), model2.lexicon))
+            total += 1
+            if pp2 <= pp1:
+                correct += 1
+
+        return correct / total
+
+if __name__ == "__main__":
+
+    model = TrigramModel(sys.argv[1]) 
+
+    # put test code here...
+    # or run the script from the command line with 
+    # $ python -i trigram_model.py [corpus_file]
+    # >>> 
+    #
+    # you can then call methods on the model instance in the interactive 
+    # Python prompt. 
+
+    
+    # Testing perplexity: 
+    # dev_corpus = corpus_reader(sys.argv[2], model.lexicon)
+    # pp = model.perplexity(dev_corpus)
+    # print(pp)
+
+
+    # Essay scoring experiment: 
+    # acc = essay_scoring_experiment('train_high.txt', 'train_low.txt", "test_high", "test_low")
+    # print(acc)
+
---
+++ HW2/nn_dep_parser/decoder.py
@@ -0,0 +1,91 @@
+import sys
+import copy
+
+import numpy as np
+import torch
+
+from conll_reader import DependencyStructure, DependencyEdge, conll_reader
+from extract_training_data import FeatureExtractor, State
+from train_model import DependencyModel
+
+class Parser(object):
+
+    def __init__(self, extractor, modelfile):
+        self.extractor = extractor
+
+        # Create a new model and load the parameters
+        self.model = DependencyModel(len(extractor.word_vocab) + len(extractor.pos_vocab), len(extractor.output_labels))
+        self.model.load_state_dict(torch.load(modelfile))
+        sys.stderr.write("Done loading model")
+
+        # The following dictionary from indices to output actions will be useful
+        self.output_labels = dict([(index, action) for (action, index) in extractor.output_labels.items()])
+
+    def parse_sentence(self, words, pos):
+
+        state = State(range(1,len(words)))
+        state.stack.append(0)
+
+        # TODO: Write the body of this loop for part 5
+        while state.buffer:
+            features = self.extractor.get_input_representation(words, pos, state)
+            features_tensor = torch.tensor(features, dtype=torch.long).unsqueeze(0)
+
+            with torch.no_grad():
+                logits = self.model(features_tensor)
+
+            # Sort actions highest score first
+            sorted_actions = torch.argsort(logits.squeeze(0), descending=True).tolist()
+
+            for action_idx in sorted_actions:
+                action, label = self.output_labels[action_idx]
+
+                if action == 'shift':
+                    # Illegal: shifting last buffer word when stack is non-empty
+                    if len(state.buffer) == 1 and len(state.stack) > 0:
+                        continue
+                    state.shift()
+                    break
+                elif action == 'left_arc':
+                    # Illegal: empty stack, or root is stack top
+                    if len(state.stack) == 0 or state.stack[-1] == 0:
+                        continue
+                    state.left_arc(label)
+                    break
+                elif action == 'right_arc':
+                    # Illegal: empty stack
+                    if len(state.stack) == 0:
+                        continue
+                    state.right_arc(label)
+                    break
+
+
+  
+
+        result = DependencyStructure()
+        for p,c,r in state.deps:
+            result.add_deprel(DependencyEdge(c,words[c],pos[c],p, r))
+
+        return result
+
+
+if __name__ == "__main__":
+
+    WORD_VOCAB_FILE = 'data/words.vocab'
+
+    try:
+        word_vocab_f = open(WORD_VOCAB_FILE,'r')
+    except FileNotFoundError:
+        print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
+        sys.exit(1)
+
+    extractor = FeatureExtractor(word_vocab_f)
+    parser = Parser(extractor, sys.argv[1])
+
+    with open(sys.argv[2],'r') as in_file:
+        for dtree in conll_reader(in_file):
+            words = dtree.words()
+            pos = dtree.pos()
+            deps = parser.parse_sentence(words, pos)
+            print(deps.print_conll())
+            print()
